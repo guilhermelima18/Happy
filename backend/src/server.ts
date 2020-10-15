dc77c41744b703fcca0728d306854811bb4ee56a
Guilherme <guilhermelima18@hotmail.com>
2020-10-15 00:11:57 -0300

    up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,38 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+import './database/connection'
+import {getRepository} from 'typeorm'
+import Orphanage from './models/Orphanage'
 
-app.listen(5000);
+const app = express()
 
-app.get("/", (req, res) => {
+app.use(express.json())
+
+app.listen(5000)
+
+app.post("/orphanages", async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = req.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    })
+
+    await orphanagesRepository.save(orphanage)
+
     return res.json({ message: "Hello World" })
 })
