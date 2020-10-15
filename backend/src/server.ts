1f28e04c462292db0c821bb73981081ea8a51f38
Guilherme <guilhermelima18@hotmail.com>
2020-10-15 09:14:51 -0300

    upload images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,9 @@
-import express from 'express'
-import './database/connection'
-import {getRepository} from 'typeorm'
-import Orphanage from './models/Orphanage'
+import express from 'express';
+import './database/connection';
+import routes from './models/routes';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-
-app.listen(5000)
-
-app.post("/orphanages", async (req, res) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = req.body
-
-    const orphanagesRepository = getRepository(Orphanage)
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    })
-
-    await orphanagesRepository.save(orphanage)
-
-    return res.json({ message: "Hello World" })
-})
+app.use(express.json());
+app.use(routes);
+app.listen(5000);
